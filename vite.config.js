0f1d2083b0c34afddf06128296c4853c16a791f0
akaoko <smagina.anyaa@gmail.com>
2025-04-04 03:40:16 +0300

    fix: update asset handling and MIME types configuration

--- vite.config.js
+++ vite.config.js
@@ -17,9 +17,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         format: "es",
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
       },
     },
   },
