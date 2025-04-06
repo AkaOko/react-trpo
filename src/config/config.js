6a060d42c3516eef45860c6383cec42609a62a5c
akaoko <smagina.anyaa@gmail.com>
2025-04-07 00:55:07 +0300

    обавлен файл конфигурации с API_ENDPOINTS

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export const API_ENDPOINTS = {
+  products: "/api/products",
+  materials: "/api/materials",
+  orders: "/api/orders",
+  users: "/api/users",
+  upload: "/api/upload",
+  productTypes: "/api/product-types",
+  materialRequests: "/api/material-requests",
+};
